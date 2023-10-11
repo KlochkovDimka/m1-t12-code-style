0928cc65459dfb9ec6b51761e499e0d513287ccb
KlochkovDimka <KlochkovDimka97@yandex.ru>
2023-10-12 02:00:55 +0300

    Отформатированный java-Класс

---
+++ src/DepositCalculator.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class DepositCalculator {
+    double CalculateComplexPercent(double amount, double yearRate, int timeStorage) {
+        double payment = amount * Math.pow((1 + yearRate / 12), 12 * timeStorage);
+        return getRandom(payment, 2);
+    }
+
+    double CalculateSimplePercent(double amount, double yearRate, int depositPeriod) {
+        return getRandom(amount + amount * yearRate * depositPeriod, 2);
+    }
+
+    double getRandom(double value, int places) {
+        double scaLe = Math.pow(10, places);
+        return Math.round(value * scaLe) / scaLe;
+    }
+
+    void doImportantJob() {
+        int timeStorage, action;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите сумму вклада в рублях:");
+        int amountDeposit = scanner.nextInt();
+        System.out.println("Введите срок вклада в годах:");
+        timeStorage = scanner.nextInt();
+        System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
+        action = scanner.nextInt();
+        double outDoubleVar = 0;
+        if (action == 1) {
+            outDoubleVar = CalculateSimplePercent(amountDeposit, 0.06, timeStorage);
+        } else if (action == 2) {
+            outDoubleVar = CalculateComplexPercent(amountDeposit, 0.06, timeStorage);
+        }
+        System.out.println("Результат вклада: " + amountDeposit + " за " + timeStorage +
+                " лет превратятся в " + outDoubleVar);
+    }
+
+    public static void main(String[] args) {
+        new DepositCalculator().doImportantJob();
+    }
+
+
+}
